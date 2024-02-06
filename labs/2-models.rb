3ebe8a5eeb236e73d4c041caf78b9f504bb86bb0
Shouvik Chaudhury <shouvik.chaudhury@kellogg.northwestern.edu>
2024-02-06 08:41:34 +0000

    commit 3

--- labs/2-models.rb
+++ labs/2-models.rb
@@ -20,16 +20,35 @@ Salesperson.destroy_all
 # 1b. check out the model file
 
 # 2. insert 1-2 rows in salespeople table.
+new_salesperson = Salesperson.new
+new_salesperson["first_name"] = "John"
+new_salesperson["last_name"] = "Doe"
+new_salesperson["email"] = "john.doe@apple.com"
+new_salesperson.save
+
+new_salesperson = Salesperson.new
+new_salesperson["first_name"] = "Albert"
+new_salesperson["last_name"] = "Tucker"
+new_salesperson.save
 
 # 3. write code to display how many salespeople rows are in the database
+puts "Salespeople: #{Salesperson.all.count}"
 
 # ---------------------------------
 # Salespeople: 2
 
 # 4. modify/update column data for a row in the salespeople table.
+person1 = Salesperson.find_by({"first_name" => "Albert", "last_name" => "Tucker"})
+person1["email"] = "albert.tucker@amazon.com"
+person1.save
 
 # CHALLENGE:
 # 5. write code to display each salesperson's full name
+salesperson_list = Salesperson.all
+
+for person in salesperson_list
+    puts "#{person["first_name"]} #{person["last_name"]}"
+end   
 
 # ---------------------------------
 # Salespeople: 2
