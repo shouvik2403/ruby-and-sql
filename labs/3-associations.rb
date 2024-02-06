3881161fb94a33ed8a586eeea2c9a40dfa18c634
Shouvik Chaudhury <shouvik.chaudhury@kellogg.northwestern.edu>
2024-02-06 11:04:41 +0000

    final commit

--- labs/3-associations.rb
+++ labs/3-associations.rb
@@ -17,34 +17,42 @@ Activity.destroy_all
 # 1. insert 3 rows in the activities table with relationships to
 # a single salesperson and 2 different contacts
 
-tim = Company.find_by({"first_name" => "Tim", "last_name" => "Cook"})
-craig = Company.find_by({"first_name" => "Craig", "last_name" => "Federighi"})
-john = Salesperson.find_by({"first_name" => "John", "last_name" => "Doe"})
-apple = Company.find_by({"name" => "Apple"})
+contact1 = Contact.find_by({"first_name" => "Tim", "last_name" => "Cook"})
+contact2 = Contact.find_by({"first_name" => "Jeff", "last_name" => "Bezos"})
+salesperson = Salesperson.find_by({"first_name" => "John", "last_name" => "Doe"})
+company1 = Company.find_by({"name" => "Apple"})
+company2 = Company.find_by({"name" => "Amazon"})
 
-activity = Activity.new
-activity["contact_id"] = tim["id"]
-activity["salesperson_id"] = john["id"]
-activity["company_id"] = apple["id"]
-activity["notes"] = "Quick checkin over Facetime"
-activity.save
+new_activity = Activity.new
+new_activity["contact_id"] = contact1["id"]
+new_activity["salesperson_id"] = salesperson["id"]
+new_activity["company_id"] = company1["id"]
+new_activity["notes"] = "Quick checkin over Facetime"
+new_activity.save
 
-activity = Activity.new
-activity["contact_id"] = tim["id"]
-activity["salesperson_id"] = john["id"]
-activity["company_id"] = apple["id"]
-activity["notes"] = "Met at Cupertino"
-activity.save
+new_activity = Activity.new
+new_activity["contact_id"] = contact1["id"]
+new_activity["salesperson_id"] = salesperson["id"]
+new_activity["company_id"] = company1["id"]
+new_activity["notes"] = "Met at Cupertino"
+new_activity.save
 
-activity = Activity.new
-activity["contact_id"] = craig["id"]
-activity["salesperson_id"] = john["id"]
-activity["company_id"] = apple["id"]
-activity["notes"] = "Presented annual plan"
-activity.save
+new_activity = Activity.new
+new_activity["contact_id"] = contact2["id"]
+new_activity["salesperson_id"] = salesperson["id"]
+new_activity["company_id"] = company2["id"]
+new_activity["notes"] = "met at Blue Origin HQ"
+new_activity.save
 
 # 2. Display all the activities between the salesperson used above
 # and one of the contacts (sample output below):
+
+activity_log = Activity.where({"salesperson_id" => salesperson["id"], "contact_id" => contact1["id"]})
+
+puts "Activities between #{salesperson["first_name"]} and #{contact1["first_name"]} #{contact1["last_name"]}"
+for activity in activity_log
+    puts "- #{activity["notes"]}"
+end
 
 # ---------------------------------
 # Activities between Ben and Tim Cook:
@@ -55,6 +63,18 @@ activity.save
 # 3. Similar to above, but display all of the activities for the salesperson
 # across all contacts (sample output below):
 
+activity_log = Activity.where({"salesperson_id" => salesperson["id"]})
+
+puts "-------------------------------------------"
+puts "#{salesperson["first_name"]}'s Activities:"
+for activity in activity_log
+    #retrieve contact name from contact id
+    first_name = Contact.find_by({"id" => activity["contact_id"]})["first_name"]
+    last_name = Contact.find_by({"id" => activity["contact_id"]})["last_name"]
+   
+    puts "#{first_name} #{last_name} - #{activity["notes"]}"
+end
+
 # ---------------------------------
 # Ben's Activities:
 # Tim Cook - quick checkin over facetime
@@ -62,6 +82,20 @@ activity.save
 # Jeff Bezos - met at Blue Origin HQ
 
 # 3a. Can you include the contact's company?
+
+puts "-------------------------------------------"
+puts "#{salesperson["first_name"]}'s Activities:"
+for activity in activity_log
+    
+    #retrieve contact name from contact id
+    first_name = Contact.find_by({"id" => activity["contact_id"]})["first_name"]
+    last_name = Contact.find_by({"id" => activity["contact_id"]})["last_name"]
+    #retreive company name
+    company_name = Company.find_by({"id" => activity["company_id"]})["name"]
+
+    puts "#{first_name} #{last_name} (#{company_name}) - #{activity["notes"]}"
+end
+
 
 # ---------------------------------
 # Ben's Activities:
@@ -71,6 +105,20 @@ activity.save
 
 # CHALLENGE:
 # 4. How many activities does each salesperson have?
+
+puts "-------------------------------------------"
+salesperson_list = Salesperson.all
+activity_log = Activity.all
+
+for people in salesperson_list
+    count_activity = 0
+    for activity in activity_log
+        if people["id"] == activity["salesperson_id"]
+            count_activity = count_activity + 1
+        end  
+    end
+    puts "#{people["first_name"]} #{people["last_name"]}: #{count_activity} activities" 
+end
 
 # ---------------------------------
 # Ben Block: 3 activities
